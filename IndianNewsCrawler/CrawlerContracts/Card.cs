6cd7bcd69c7e81936f0c7730f823f8aed5402fd8
rapanuga <rapanuga@microsoft.com>
2015-11-28 15:57:06 -0800

    Add Contracts to Crawlers

---
+++ IndianNewsCrawler/CrawlerContracts/Card.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerContracts
+{
+    [DataContract]
+    public class Card
+    {
+        [DataMember]
+        public string Guid { get; set; }
+        [DataMember]
+        public string ShortStoryId { get; set; }
+        [DataMember]
+        public string ExpandedCardId { get; set; }
+        [DataMember]
+        public string SocialCardId { get; set; }
+    }
+
+    [DataContract]
+    public class SocialCard
+    {
+        [DataMember]
+        public string Guid { get; set; }
+        [DataMember]
+        public int Likes { get; set; }
+        [DataMember]
+        public int Shares { get; set; }
+    }
+
+    [DataContract]
+    public class ShortStory
+    {
+        [DataMember]
+        public string Guid { get; set; }
+        [DataMember]
+        public string ImageUrl { get; set; }
+        [DataMember]
+        public string Headline { get; set; }
+        [DataMember]
+        public string Summary { get; set; }
+        [DataMember]
+        public string Source { get; set; }        
+    }
+
+    [DataContract]
+    public class ExpandedCard
+    {
+        public string Guid { get; set; }
+        public string Headline { get; set; }
+        public string ImageUrl { get; set; }
+        public string Body { get; set; }
+    }
+
+}
